ff7e68608a8bb59cccfd37ac9effa8b8b02048b6
sloppy-psd <95029486+sloppy-psd@users.noreply.github.com>
2024-02-09 20:09:58 +0300

    Changed Link to project

--- js/draggable_box_mobile.js
+++ js/draggable_box_mobile.js
@@ -70,5 +70,5 @@ hideButton2.addEventListener('touchend', (e) => {
 img_main_page.addEventListener('touchend', (e) => {
   e.preventDefault();
   // Assuming there is a function handleButtonClick defined somewhere
-  handleButtonClick('projects_vetvi.html');
+  handleButtonClick('project_ClanSix.html');
 });
